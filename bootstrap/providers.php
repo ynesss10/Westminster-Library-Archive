6a8e1174d6737f22b956250760f0aa7c58ec2476
davinjonas123-code <davinjonas123@gmail.com>
2026-09-01 20:49:36 +0700

    Revert "Feature/admin frontend"

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
+use App\Providers\AppServiceProvider;
+
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\FortifyServiceProvider::class,
+    AppServiceProvider::class,
 ];
