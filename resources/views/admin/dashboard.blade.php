5167cbced8cc831a360ec615f40693f2ad0df22a
davinjonas123-code <davinjonas123@gmail.com>
2026-09-01 20:20:10 +0700

    feat: Partials for header and footer

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard - WestMinster</title>
-</head>
+@section('title', 'Admin Dashboard')
 
-<body>
+@section('content')
 
     <h1>Admin Dashboard</h1>
 
@@ -55,6 +50,4 @@
         Kelola Peminjaman
     </a>
 
-</body>
-
-</html>
+@endsection
